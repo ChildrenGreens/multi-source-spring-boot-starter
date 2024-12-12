18591601fd801d9eb03bfe59b1d3aa7160d974e0
childrengreens <childrengreens@163.com>
2024-12-12 23:27:02 +0800

    Updating license header

--- redis-multi-source-spring-boot-starter/src/main/java/com/childrengreens/multi/source/PropertiesRedisMultiSourcesConnectionDetails.java
+++ redis-multi-source-spring-boot-starter/src/main/java/com/childrengreens/multi/source/PropertiesRedisMultiSourcesConnectionDetails.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.childrengreens.multi.source;
 
 import com.childrengreens.multi.source.RedisMultiSourcesConnectionConfiguration.ConnectionInfo;
